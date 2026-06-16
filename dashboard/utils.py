e76c13415d7f08c78d18b53191e8efe68b4a823f
ShlokBagwe <shlokbagwe.tech@gmail.com>
2026-06-16 18:06:16 +0530

    Updated utils.py

--- dashboard/utils.py
+++ dashboard/utils.py
@@ -1,31 +1,35 @@
 import streamlit as st
 import pandas as pd
 import joblib
+from pathlib import Path
+
+
+BASE_DIR = Path(__file__).parent
 
 
 @st.cache_data
 def load_data():
-    return pd.read_csv("cord_real_estate.csv")
+    return pd.read_csv(BASE_DIR / "cord_real_estate.csv")
 
 
 @st.cache_resource
 def load_model():
-    return joblib.load("best_xgb_model_v2.joblib")
+    return joblib.load(BASE_DIR / "best_xgb_model_v2.joblib")
 
 
 @st.cache_resource
 def load_model2():
-    return joblib.load("best_bayesian_xgb_model.joblib")
+    return joblib.load(BASE_DIR / "best_bayesian_xgb_model.joblib")
 
 
 # Recommender System
 def load_recommender_data():
-    return pd.read_csv("Final RS Dataset.csv")
+    return pd.read_csv(BASE_DIR / "Final RS Dataset.csv")
 
 @st.cache_resource
 def load_recommender_model():
-    return joblib.load("latest_weighted_nn_model.joblib")
+    return joblib.load(BASE_DIR / "latest_weighted_nn_model.joblib")
 
 @st.cache_resource
 def load_recommender_matrix():
-    return joblib.load("latest_X_weighted_matrix.joblib")
+    return joblib.load(BASE_DIR / "latest_X_weighted_matrix.joblib")
